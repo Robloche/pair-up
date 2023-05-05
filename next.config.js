6f7a293edcd2f44aa02c00f4e47c46bc03e8efd7
Rodolphe Breton <rodolphe.breton@netgem.com>
2023-05-05 08:02:30 +0200

    Made app a PWA (close #5)

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+});
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withPWA({
+  images: { unoptimized: true },
   productionBrowserSourceMaps: true,
   reactStrictMode: true,
-};
+});
 
 module.exports = nextConfig;
